95334e592d28692376ee9e538a20af67d3b04f81
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-08-10 15:24:32 +0200

    doc: Improve docstring of ActionType

--- python/rrc_simulation/gym_wrapper/envs/cube_env.py
+++ python/rrc_simulation/gym_wrapper/envs/cube_env.py
@@ -63,25 +63,21 @@ class FixedInitializer:
 
 
 class ActionType(enum.Enum):
-    """Different action types that can be used to control the robot.
+    """Different action types that can be used to control the robot."""
 
-    The different types are:
-        TORQUE: Use pure torque commands.  The action is a list of torques (one
-            per joint) in this case.
-        POSITION: Use joint position commands.  The action is a list of angular
-            joint positions (one per joint) in this case.  Internally a PD
-            controller is executed for each action to determine the torques
-            that are applied to the robot.
-        TORQUE_AND_POSITION: Use both torque and position commands.  In this
-            case the action is a dictionary with keys "torque" and "position"
-            which contain the corresponding lists of values (see above).  The
-            torques resulting from the position controller are added to the
-            torques in the action before applying them to the robot.
-
-    """
-
+    #: Use pure torque commands.  The action is a list of torques (one per
+    #: joint) in this case.
     TORQUE = enum.auto()
+    #: Use joint position commands.  The action is a list of angular joint
+    #: positions (one per joint) in this case.  Internally a PD controller is
+    #: executed for each action to determine the torques that are applied to
+    #: the robot.
     POSITION = enum.auto()
+    #: Use both torque and position commands.  In this case the action is a
+    #: dictionary with keys "torque" and "position" which contain the
+    #: corresponding lists of values (see above).  The torques resulting from
+    #: the position controller are added to the torques in the action before
+    #: applying them to the robot.
     TORQUE_AND_POSITION = enum.auto()
 
 
